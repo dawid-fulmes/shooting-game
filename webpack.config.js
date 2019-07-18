fa6f81bbb291544e2d92ae5ea359609f5113ad60
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-07-18 13:24:29 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[contentHash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
